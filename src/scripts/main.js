0aa958ad8cc641e093c5439e35be9738bb4612fd
paweltar <paweltar@gmail.com>
2017-04-21 13:00:44 +0200

    Add slick slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
 UIkit.use(Icons);
 
 $(document).ready(function() {
   console.log('hello');
+
+  $('.last-projects__slider').slick({
+    dots: true,
+    speed: 500
+  });
 });
