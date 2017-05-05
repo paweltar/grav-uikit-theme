d09829d7735e99455ad64b30449a0106f448b82d
paweltar <paweltar@gmail.com>
2017-05-05 15:32:15 +0200

    Dictionary page in progress

--- js/main.js
+++ js/main.js
@@ -75,20 +75,42 @@
 	_uikit2.default.use(_uikitIcons2.default);
 
 	(0, _jquery2.default)(document).ready(function () {
-	  console.log('hello');
+	    console.log('hello');
 
-	  (0, _jquery2.default)('.last-projects__slider').slick({
-	    speed: 500,
-	    slidesToShow: 1,
-	    slidesToScroll: 1,
-	    autoplay: true,
-	    autoplaySpeed: 4000,
-	    prevArrow: ".slider__button--prev",
-	    nextArrow: ".slider__button--next",
-	    fade: true,
-	    cssEase: 'linear'
-	  });
+	    (0, _jquery2.default)('.last-projects__slider').slick({
+	        speed: 500,
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        autoplay: true,
+	        autoplaySpeed: 4000,
+	        prevArrow: ".slider__button--prev",
+	        nextArrow: ".slider__button--next",
+	        fade: true,
+	        cssEase: 'linear'
+	    });
 	});
+
+	window.clearSearchBox = function (query) {
+	    var searchBox = document.getElementById('dictionary-search-input');
+	    console.log(query);
+	    searchBox.value = query;
+	    triggerEvent(searchBox, 'keyup');
+	    return true;
+	};
+
+	window.triggerEvent = function (el, type) {
+	    if ('createEvent' in document) {
+	        // modern browsers, IE9+
+	        var e = document.createEvent('HTMLEvents');
+	        e.initEvent(type, false, true);
+	        el.dispatchEvent(e);
+	    } else {
+	        // IE 8
+	        var e = document.createEventObject();
+	        e.eventType = type;
+	        el.fireEvent('on' + e.eventType, e);
+	    }
+	};
 
 /***/ }),
 /* 1 */
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,39 @@ import slickCss from 'slick-carousel/slick/slick.css';
 UIkit.use(Icons);
 
 $(document).ready(function() {
-  console.log('hello');
+    console.log('hello');
 
-  $('.last-projects__slider').slick({
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    prevArrow: ".slider__button--prev",
-    nextArrow: ".slider__button--next",
-    fade: true,
-    cssEase: 'linear'
-  });
+    $('.last-projects__slider').slick({
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: ".slider__button--prev",
+        nextArrow: ".slider__button--next",
+        fade: true,
+        cssEase: 'linear'
+    });
 });
+
+window.clearSearchBox = function(query) {
+    var searchBox = document.getElementById('dictionary-search-input');
+    console.log(query);
+    searchBox.value = query;
+    triggerEvent(searchBox, 'keyup');
+    return true;
+}
+
+window.triggerEvent = function(el, type){
+   if ('createEvent' in document) {
+        // modern browsers, IE9+
+        var e = document.createEvent('HTMLEvents');
+        e.initEvent(type, false, true);
+        el.dispatchEvent(e);
+    } else {
+        // IE 8
+        var e = document.createEventObject();
+        e.eventType = type;
+        el.fireEvent('on'+e.eventType, e);
+    }
+}
